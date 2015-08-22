6e4e2736348534cbc3316b913f6bcfcea2a510d3
Arjun Bhatnagar <me@arjunb.com>
2015-08-22 13:21:27 -0400

    Added site

---
+++ js/date.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.datepicker').pickadate({
+        format: 'On EE'
+    });
+      
+    $('.timepicker').pickatime({
+    
+    });
+    //$('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
